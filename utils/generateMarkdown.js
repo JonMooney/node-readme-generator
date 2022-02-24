126c1f66f17da8edb0ef6e166a73d1dbda697685
Jon Mooney <jon.mooney@yahoo.com>
2022-02-23 16:57:07 -0800

    updated generatemarkdown function, updated sample data object with mock data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if(!license){
+    return '';
+  }
+  
   switch(license){
     case 'MIT': return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
     case 'GPL v3': return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/)';
@@ -8,48 +11,104 @@ function renderLicenseBadge(license) {
 
 }
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return '';
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  switch(license){
+    case 'MIT': return 'This project is covered under the [MIT License](https://opensource.org/licenses/MIT)';
+    case 'GPL v3': return 'This project is covered under the [GNU General Public License v3.0](https://www.gnu.org/licenses/)';
+  }
+}
 
-function generateToc(){
+function renderToc(){
 
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
+  // Title (required)
+  const markdown = `# ${data.title}`;
 
-  ${renderLicenseBadge(data.license)}
+  // License Badge (if user selected a license) (boolean)
+  if(data.license){
+    markdown += renderLicenseBadge(data.license);
+  }
 
-  ## Description
+  // Desription (required)
+  markdown += `
+    ## Description
+    ${data.description}
+    `;
   
-  ${data.description}
-
+// Table of contents (boolean)
+if(data.toc){
+  markdown += `
   ## Table of Contents
-
-  
+  ${renderToc(data)}
   `;
-  //Title
-  //Description
-  //Table of Contents
-  //Installation
-  //Usage
-  //License
-  //Contributing
-  //Tests
-  //Common Questions
+}
+
+// Installation
+if(data.installation != ''){
+  markdown += `
+  ## Installation
+  ${data.installation}
+  `;
+}
+
+// Usage
+if(data.usage != ''){
+  markdown += `
+  ## Usage
+  ${data.usage}
+  `;
+}
+
+// License
+if(data.license){}
+  markdown += `
+  ## License
+  ${renderLicenseSection(data.license)}
+  `;
+}
+
+// Contributing
+if(data.contributing != ''){
+  markdown += `
+  ## Contributing
+  ${data.contributing}
+  `;
+}
+
+// Tests
+if(data.tests != ''){
+  markdown += `
+  ## Tests
+  ${data.tests}
+  `;
+}
+
+// Common Questions
+if(data.questions != ''){
+  markdown += `
+  ## Common Questions
+  ${data.questions}
+  `;
 }
 
 const data = {
-  title: "Project Title",
+  title: "Sample Project Title",
   license: 'GPL v3',
-  description: "Sample description"
+  description: "Sample description",
+  toc: true,
+  installation: 'sample install info',
+  usage: 'sample usage info',
+  contributing: 'contributing info'
+  tests: 'sample tests'
+  questions: 'sample questions'
+
 }
 
 console.log(generateMarkdown(data));
